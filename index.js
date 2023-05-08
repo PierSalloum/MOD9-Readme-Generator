46586e55142c17dcb6339df99b4e940876ba714a
Pier Salloum <piersalloum@gmail.com>
2023-05-08 00:47:00 -0700

    Added badges to Readme

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 
+// calling modules to file
 const inquirer = require("inquirer")
 const fs = require("fs");
 
@@ -56,32 +57,63 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
+// use fs module t
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (err) => {
-        if (err) {
-            console.error(err);
-            return;
-        }
-        console.log('File created successfully!');
+        if (err) {console.error(err);return;}
+        console.log('File created successfully');
     });
 }
-
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
+    //use inquire module to prompt user and receive data
         .prompt(questions)
-
         .then((answers) => {
-            const fileName = answers.title + '.txt'; // Generate the file name
-            const fileContent = `Title: ${answers.title}\nDescription: ${answers.description}\nUsage: ${answers.usage}\nContribution: ${answers.contribution}\nTesting: ${answers.test}\nLicense: ${answers.license}\nQuestions:\n ${answers.github}\n${answers.email}`; // Generate the file content
-            writeToFile(fileName, fileContent); // Write the file
+            const fileName = answers.title + ".md";
+            //add badge to license
+            let licenseBadge = "";
+            switch (answers.license) {
+                case "Apache 2.0": licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"; break;
+                case "MIT": licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"; break;
+                case "GNU GPLv3": licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"; break;
+                case "BSD 3-Clause": licenseBadge = "[![License: BSD 3-Clause](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"; break;
+                default:
+                    licenseBadge = "";
+            }
+            const fileContent =
+            //layout of readme with user data
+                `# ${answers.title}\n\n` +
+                `## Table of Contents\n\n` +
+                `- [Description](#description)\n` +
+                `- [Installation](#installation)\n` +
+                `- [Usage](#usage)\n` +
+                `- [Contribution](#contribution)\n` +
+                `- [Testing](#testing)\n` +
+                `- [License](#license)\n` +
+                `- [Questions](#questions)\n\n` +
+                `## Description<a name="description"></a>\n\n` +
+                `${answers.description}\n\n` +
+                `## Installation<a name="installation"></a>\n\n` +
+                `${answers.installation}\n\n` +
+                `## Usage<a name="usage"></a>\n\n` +
+                `${answers.usage}\n\n` +
+                `## Contribution<a name="contribution"></a>\n\n` +
+                `${answers.contribution}\n\n` +
+                `## Testing<a name="testing"></a>\n\n` +
+                `${answers.testing}\n\n` +
+                `## License<a name="license"></a>\n\n` +
+                `${licenseBadge}\n\n` +
+                `This project is licensed under the ${answers.license} license.\n\n` +
+                `## Questions<a name="questions"></a>\n\n` +
+                `[${answers.email}].[${answers.github}](https://github.com/${answers.github}/).\n`;
+            writeToFile(fileName, fileContent);
         })
         .catch((error) => {
             console.error(error);
         });
 }
-
 
 // Function call to initialize app
 init();
